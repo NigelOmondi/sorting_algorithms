2870ebd38d4882d9b425b4ebd281ed912bbf1c9b
Nigel Omondi <nigelomondi62@gmail.com>
2023-06-24 14:38:50 +0300

    Sort an array of integers in ascending order using the Bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - Sorts an array of integers
+ * in ascending order using Bubble sort.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int swapped;
+
+	if (array == NULL || size <= 1)
+		return;
+
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				swapped = 1;
+			}
+		}
+
+		printf("%d", array[0]);
+		for (j = 1; j < size; j++)
+			printf(", %d", array[j]);
+		printf("\n");
+
+		if (swapped == 0)
+			break;
+	}
+}
